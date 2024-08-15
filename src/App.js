bbe261c2ea831071eb2671719d32761998ca61b8
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2024-08-15 19:52:03 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Phonebook from './components/phonebook';
+function App() {
+  return (
+    <div className="App">
+     <Phonebook />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/phonebook.jsx
@@ -0,0 +1,14 @@
+import React ,{useState, useEffect} from "react";
+
+const Phonebook = () => {
+
+
+
+    return (
+        <div>
+            <h1>siema</h1>
+        </div>
+    )
+}
+
+export default Phonebook
