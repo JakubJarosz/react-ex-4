eee77e00774bc1740b5a38a86c3d64de98f63170
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2024-08-15 22:01:04 +0200

    j

--- src/components/phonebook.jsx
+++ src/components/phonebook.jsx
@@ -1,13 +1,43 @@
 import React ,{useState, useEffect} from "react";
 
 const Phonebook = () => {
+const [namelist, setNamelist] = useState('');   
+const [phonelist, setPhonelist] = useState('');
+const [fullList, setFullList] = useState([]);
 
+const handleName = (e) => {
+    setNamelist(e.target.value)
+}
+
+const handlePhone = (e) => {
+    setPhonelist(e.target.value);
+}
+
+
+  
+const onBtnPress = (e) => {
+    e.preventDefault();
+    const listArr = 
+        {
+            "name": namelist,
+            "phone": phonelist
+        }
+    setFullList(fullList.push(listArr))
+    console.log(fullList)
+  
+}
 
 
     return (
-        <div>
-            <h1>siema</h1>
-        </div>
+       <div>
+          <form onSubmit={onBtnPress}> 
+        <label>Name</label><br></br>
+        <input type="text" onChange={handleName} /><br></br>
+        <label>Phone</label><br></br>
+        <input type="text" onChange={handlePhone} /><br></br>
+        <button type="submit">Submit</button>
+      </form>
+       </div>
     )
 }
 
